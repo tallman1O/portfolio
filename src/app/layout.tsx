684a8177bb9c775614674fd4aaa6256c0a84aed2
Mehul Uttam <mehulsavio@gmail.com>
2025-05-29 03:18:37 +0530

    fix: remove custom icon from metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,6 @@ export const metadata: Metadata = {
   title: "Mehul Uttam - Developer",
   description:
     "Mehul Uttam is a developer, designer and a tech savvy. He is a digital nomad and prefers working remotely.",
-  icons: {
-    icon: "/mehul.png",
-  },
 };
 
 export default function RootLayout({
